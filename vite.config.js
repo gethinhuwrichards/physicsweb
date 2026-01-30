c66b120bb9f93c131d6d91ae61540dbb6eb4d32a
gethinhuwrichards <gethinhuwrichards@gmail.com>
2026-01-30 23:38:33 +0400

    Fix Vite app entry and build output js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env.NODE_ENV': JSON.stringify('production'),
-  },
-  build: {
-    lib: {
-      entry: 'src/main.jsx',
-      name: 'QuestionView',
-      fileName: 'question-view',
-      formats: ['iife'],
-    },
-    outDir: 'dist',
-  },
+  plugins: [react()]
 });
